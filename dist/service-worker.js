e4b590a547c70021e78e1929784bde0dac3469fb
Vinit Tomar <vinit.tomar68@gmail.com>
2020-06-30 00:08:09 +0530

    build 2

--- dist/service-worker.js
+++
@@ -1,9 +0,0 @@
-self.addEventListener('install', event => {
-  // console.log('Service worker installing...');
-  // cache your resources here.
-  self.skipWaiting();
-});
-
-self.addEventListener('activate', event => {
-  // console.log('Service worker activating...');
-});
