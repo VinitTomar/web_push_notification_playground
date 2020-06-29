6ec4c783c054be27327b7a7e4da2aa65732b9e14
Vinit Tomar <vinit.tomar68@gmail.com>
2020-06-30 00:04:02 +0530

    service worker path updated

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -16,7 +16,7 @@ export class AppComponent {
   constructor() {
     if ('serviceWorker' in navigator) {
       window.addEventListener('load', () => {
-        navigator.serviceWorker.register('/service-worker.js')
+        navigator.serviceWorker.register('service-worker.js')
           .then(registration => {
             // console.log('Service worker registered', registration.scope);
           })
